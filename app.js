13463d5dd6d43edbf69bbed41e4b74babf74c198
ns.apon@outlook.com <bashir.vu@gmail.com>
2021-04-11 14:26:26 +0600

    Complete Bus ticket booking app

--- app.js
+++ app.js
@@ -123,8 +123,8 @@ const ConditionalsandLoops = {
     computed: {
         total_price() {
             return this.products.reduce((sum, product) => {
-                return sum +=product.price;
-            },0);
+                return sum += product.price;
+            }, 0);
         }
     },
     methods: {
@@ -148,17 +148,298 @@ const ConditionalsandLoops = {
 const InvoiceProject = {
     data() {
         return {
-            items: [
-                { description: "Website design", quantity: 1, price: 300 }
-              ]
+            items: [{
+                description: "Website design",
+                quantity: 1,
+                price: 300
+            }]
         }
     },
     methods: {
         addRow() {
-            this.items.push({ description: "", quantity: 1, price: 0 });
+            this.items.push({
+                description: "",
+                quantity: 1,
+                price: 0
+            });
         },
         removeRow(i) {
             this.items.splice(i, 1)
+        }
+
+    }
+}
+
+const WatcherApp = {
+    data() {
+        return {
+            mobile: '',
+            message: ''
+        }
+    },
+    watch: {
+        mobile(newValue) {
+            if (isNaN(newValue)) {
+                this.message = "Please enter valid mobile no.";
+            } else {
+                this.message = "";
+            }
+        }
+    },
+    methods: {
+
+    }
+}
+
+const TicketApp = {
+    data() {
+        return {
+            ButtonStatus: false,
+            confirmed: false,
+            name: '',
+            mobile: '',
+            appliedCoupon: null,
+            couponCode: "",
+            coupons: [{
+                    code: "100TAKAOFF",
+                    discount: 100
+                },
+                {
+                    code: "200TAKAOFF",
+                    discount: 200
+                }
+            ],
+            seatStates: {
+                sold: {
+                    text: "Sold",
+                    color: "#ff0000"
+                },
+                available: {
+                    text: "Available",
+                    color: "#fff"
+                },
+                booked: {
+                    text: "Booked",
+                    color: "gray"
+                },
+                selected: {
+                    text: "Selected",
+                    color: "#00ff00"
+                }
+            },
+            seats: [{
+                    name: "A1",
+                    type: "available",
+                    price: 500
+                },
+                {
+                    name: "A2",
+                    type: "available",
+                    price: 500
+                },
+                {
+                    name: "A3",
+                    type: "available",
+                    price: 500
+                },
+                {
+                    name: "A4",
+                    type: "available",
+                    price: 500
+                },
+                {
+                    name: "B1",
+                    type: "available",
+                    price: 450
+                },
+                {
+                    name: "B2",
+                    type: "available",
+                    price: 450
+                },
+                {
+                    name: "B3",
+                    type: "available",
+                    price: 450
+                },
+                {
+                    name: "B4",
+                    type: "available",
+                    price: 450
+                },
+                {
+                    name: "C1",
+                    type: "sold",
+                    price: 500
+                },
+                {
+                    name: "C2",
+                    type: "sold",
+                    price: 500
+                },
+                {
+                    name: "C3",
+                    type: "sold",
+                    price: 500
+                },
+                {
+                    name: "C4",
+                    type: "sold",
+                    price: 500
+                },
+                {
+                    name: "D1",
+                    type: "available",
+                    price: 400
+                },
+                {
+                    name: "D2",
+                    type: "available",
+                    price: 400
+                },
+                {
+                    name: "D3",
+                    type: "available",
+                    price: 400
+                },
+                {
+                    name: "D4",
+                    type: "available",
+                    price: 400
+                },
+                {
+                    name: "E1",
+                    type: "available",
+                    price: 300
+                },
+                {
+                    name: "E2",
+                    type: "available",
+                    price: 300
+                },
+                {
+                    name: "E3",
+                    type: "booked",
+                    price: 300
+                },
+                {
+                    name: "E4",
+                    type: "booked",
+                    price: 300
+                },
+                {
+                    name: "F1",
+                    type: "available",
+                    price: 300
+                },
+                {
+                    name: "F2",
+                    type: "available",
+                    price: 300
+                },
+                {
+                    name: "F3",
+                    type: "available",
+                    price: 300
+                },
+                {
+                    name: "F4",
+                    type: "available",
+                    price: 300
+                }
+            ]
+
+        }
+    },
+    mounted() {
+        this.$watch(vm => [vm.name, vm.mobile], val => {
+            if (this.name != '' && this.mobile != '') {
+                this.ButtonStatus = true;
+            } else {
+                this.ButtonStatus = false;
+            }
+
+        }, {
+            immediate: true, // run immediately
+            deep: true // detects changes inside objects. not needed here, but maybe in other cases
+        })
+    },
+    computed: {
+        selectedSeats() {
+            let sc = this.seats.filter((item) => item.type === "selected");
+            return sc;
+        },
+        totalCost() {
+            let tc = 0;
+            this.selectedSeats.forEach((seat) => {
+                tc += seat.price;
+            });
+
+            if (this.appliedCoupon !== null) {
+                tc = tc - this.appliedCoupon.discount;
+            }
+
+            return tc;
+        },
+
+
+    },
+    watch: {
+        couponCode(newValue) {
+            if (newValue.length === 10) {
+                let searchedCoupons = this.coupons.filter(
+                    (item) => item.code === newValue
+                );
+
+                if (searchedCoupons.length === 1) {
+                    this.appliedCoupon = searchedCoupons[0];
+                    this.couponCode = "";
+                } else {
+                    alert("Coupon not valid!");
+                }
+            }
+        },
+
+    },
+    methods: {
+        handleClick(i) {
+            let clickedSeat = this.seats[i];
+
+            // prevent select: sold & booked seat
+            if (clickedSeat.type === "sold" || clickedSeat.type === "booked") {
+                alert("You can not select this seat");
+                return;
+            }
+
+            // prevent more then 3 seat select
+            if (clickedSeat.type == "available" && this.selectedSeats.length > 2) {
+                alert("You can not select more than 3 seats");
+                return;
+            }
+
+            // For deselect
+            clickedSeat.type = clickedSeat.type === "selected" ? "available" : "selected";
+            console.log(clickedSeat);
+        },
+        confirm() {
+            if (!this.name || !this.mobile) {
+                alert("Please enter name and mobile");
+                return;
+            }
+
+            this.confirmed = true;
+        },
+        resetData() {
+            this.confirmed = false;
+            this.name = "";
+            this.mobile = "";
+            this.appliedCoupon = null;
+
+            this.seats.forEach((seat) => {
+                if (seat.type === "selected") {
+                    seat.type = "sold";
+                }
+            });
         }
 
     }
@@ -168,3 +449,5 @@ Vue.createApp(event_modifier).mount('#event_modifier')
 Vue.createApp(TowWayDataBinding).mount('#TowWayDataBinding')
 Vue.createApp(ConditionalsandLoops).mount('#ConditionalsandLoops')
 Vue.createApp(InvoiceProject).mount('#InvoiceProject')
+Vue.createApp(WatcherApp).mount('#WatcherApp')
+Vue.createApp(TicketApp).mount('#TicketApp')
